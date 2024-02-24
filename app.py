ac4a5cf4c79b0a2a0cb2f26755009a881257d582
Julieta <julieta.digifico@gmail.com>
2024-02-24 18:42:17 -0300

    Ejercicio3 for

---
+++ app.py
@@ -0,0 +1,46 @@
+import tkinter
+from tkinter.messagebox import showinfo as alert
+from tkinter.messagebox import askyesno as question
+from tkinter.simpledialog import askstring as prompt
+import customtkinter
+
+'''
+nombre: Julieta
+apellido: Digifico
+---
+Ejercicio: for_03
+---
+Enunciado:
+Al presionar el botón 'Mostrar' tomar el valor de la caja de texto (txt_repetir) y mostrar el mensaje "Hola UTN FRA" la cantidad de veces que el usuario ingreso (utilizando el Dialog Alert)
+'''
+
+class App(customtkinter.CTk):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.title("UTN Fra")
+
+        self.label1 = customtkinter.CTkLabel(master=self, text="Repetir")
+        self.label1.grid(row=0, column=0, padx=20, pady=10)
+        
+        self.txt_repetir = customtkinter.CTkEntry(master=self)
+        self.txt_repetir.grid(row=0, column=1)
+                
+        self.btn_mostrar = customtkinter.CTkButton(master=self, text="Mostrar", command=self.btn_mostrar_on_click)
+        self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
+
+
+    def btn_mostrar_on_click(self):
+        Saludo = self.txt_repetir.get()
+        if Saludo.isdigit():
+            Saludo = int(Saludo)
+            for _ in range(Saludo):
+                alert("Mensaje", "Hola UTN FRA")
+        else:
+            alert("Error", "Por favor, indicar numeros validos")
+    
+if __name__ == "__main__":
+    app = App()
+    app.geometry("300x300")
+    app.mainloop()
